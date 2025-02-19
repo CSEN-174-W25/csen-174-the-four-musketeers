c32b78fc51bca1c68c60824e76e241b672069b90
muti1967 <mutishuman@gmail.com>
2025-02-19 13:12:03 -0800

    test2
    push

--- src/script.js
+++ src/script.js
@@ -7,7 +7,7 @@ function setupFormSubmission() {
         const messageDiv = document.getElementById('message');
 
         if (!content) {
-            messageDiv.textContent = 'Failed to create PDF.';
+            messageDiv.textContent = 'Failed to create the PDF.';
             return;
         }
 
